c3d73bd960766baac23ca52fed62cc32725c0a26
Angel Reyes V. <rvangelse@gmail.com>
2024-09-14 11:47:06 -0300

    Avance en la guia

--- Practica1.hs
+++ Practica1.hs
@@ -1,3 +1,4 @@
+import GHC.Num.BigNat (bigNatAdd)
 
 max2 :: Float -> Float -> Float
 max2 x y | x >= y = x
@@ -69,7 +70,81 @@ recr _ z [] = z
 recr f z (x : xs) = f x xs (recr f z xs)
 
 sacarUna :: Eq a => a -> [a] -> [a]
-sacarUna y = recr (\x xs rec -> if x == y then xs else y: rec) []
+sacarUna y = recr (\x xs rec -> if x == y then xs else x: rec) []
+
 --No podemos usar foldr porque, necesitamos una recursion primitiva que nos permita acceder a mas informacion de las subestructuras recursivas.
+
 insertarOrdenado :: Ord a => a -> [a] -> [a]
 insertarOrdenado y = recr (\x xs rec  ->if y <= x then y:x:xs else x:rec) [y]
+
+mapPares :: (a -> b -> c) -> [(a, b)] -> [c]
+mapPares f = map (Main.uncurry f)
+
+armarPares :: [a] -> [b] -> [(a, b)] -- Zip
+armarPares = foldr (\x rec ys -> if null ys then [] else (x, head ys) : rec (tail ys)) (const [])
+
+mapDoble :: (a -> b -> c) -> [a] -> [b] -> [c] -- Zipwith
+mapDoble f = foldr (\x rec ys -> if null ys then [] else f x (head ys) : rec (tail ys)) (const [])
+
+foldNat :: (Integer -> b -> b) -> b -> Integer -> b
+foldNat f z 0 = z
+foldNat f z n = f n (foldNat f z (n-1))
+
+potencia :: Integer -> Integer -> Integer
+potencia x = foldNat (\y rec -> rec * x) 1
+
+data AB a = Nil | Bin (AB a) a (AB a) deriving(Show)
+
+foldAB ::b -> (b -> a -> b -> b) -> AB a -> b
+foldAB z f x = case x of
+            Nil -> z
+            (Bin l v r) -> f (rec l) v (rec r)
+    where rec = foldAB z f
+
+recAB :: b -> (AB a -> a -> AB a -> b -> b -> b) -> AB a -> b
+recAB z f x = case x of
+            Nil -> z
+            (Bin l v r) -> f l v r (rec l) (rec r)
+    where rec = recAB z f
+
+esNil :: AB a -> Bool
+esNil x = case x of
+    Nil -> True
+    _ -> False
+
+altura :: AB a -> Integer
+altura = foldAB 0 (\rl v rr -> 1 + max rl rr)
+
+cantNodos :: AB a -> Integer
+cantNodos = foldAB 0 (\rl v rr -> 1 + rl + rr)
+
+mejorSegún :: (a -> a -> Bool) -> AB a -> a
+mejorSegún f (Bin l v r) = foldAB v (\rl v rr -> (rl `g` v) `g` rr) (Bin l v r)
+    where g x y = if f x y then x else y
+
+esABB :: Ord a => AB a -> Bool
+esABB = recAB True f
+    where f l v r rl rr | esNil l && esNil r = True
+                        | esNil r = rl && raíz l <= v
+                        | esNil l = rr && v < raíz r
+                        | otherwise = rl && rr && raíz l <= v && v < raíz r
+
+raíz :: AB a -> a
+raíz (Bin l v r) = v
+
+data RoseTree a = Rose a [RoseTree a]
+
+foldRose :: ( a -> [b] -> b ) -> RoseTree a -> b
+foldRose cRose (Rose n hijos) = cRose n (map rec hijos)
+    where rec = foldRose cRose
+
+hojas :: RoseTree a -> [a]
+hojas = foldRose (\n hijos -> if null hijos then [n] else concat hijos)
+
+distancias :: RoseTree a -> [Int]
+distancias = foldRose (\_ hijos -> if null hijos then [0] else map (+1) (concat hijos))
+
+alturaRT :: RoseTree a -> Int
+alturaRT = foldRose (\_ hijos -> 1 + maximum (0 : hijos))
+
+
