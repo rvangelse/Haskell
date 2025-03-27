9bb22dde6222e78da5c66d0128915256b3569469
Angel Reyes V. <rvangelse@gmail.com>
2025-03-26 23:37:08 -0300

    Agrego ejercicios de la práctica 1

--- Practica1.hs
+++ Practica1.hs
@@ -1,21 +1,3 @@
-import GHC.Num.BigNat (bigNatAdd)
-
-max2 :: Float -> Float -> Float
-max2 x y | x >= y = x
-         | otherwise = y
-
-normaVectorial :: Float -> Float -> Float
-normaVectorial x y = sqrt (x^2 + y^2)
-
-subtract :: Float -> Float -> Float
-subtract = flip (-)
-
-predecesor :: Float -> Float 
-predecesor  = Main.subtract 1 
-
-evaluarEnCero :: (Float -> b) -> b
-evaluarEnCero = \f -> f 0
-
 dosVeces :: (a -> a) -> a -> a
 dosVeces = \f -> f . f
 
